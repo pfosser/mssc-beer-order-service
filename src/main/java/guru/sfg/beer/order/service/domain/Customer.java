b8bc0a46e8dc1690f442a1ff34ab9d7bf01c94b2
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2025-11-05 09:51:52 +0100

    Lessons 163-164  Configure Services for MariaDB

--- src/main/java/guru/sfg/beer/order/service/domain/Customer.java
+++ src/main/java/guru/sfg/beer/order/service/domain/Customer.java
@@ -16,17 +16,20 @@
  */
 package guru.sfg.beer.order.service.domain;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.sql.Timestamp;
+import java.util.Set;
+import java.util.UUID;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
-import java.sql.Timestamp;
-import java.util.Set;
-import java.util.UUID;
+
+import org.hibernate.annotations.Type;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * Created by jt on 2019-01-26.
@@ -48,7 +51,8 @@ public class Customer extends BaseEntity {
 
     private String customerName;
 
-    @Column(length = 36, columnDefinition = "varchar")
+    @Column(length = 36, columnDefinition = "varchar(36)")
+    @Type(type="org.hibernate.type.UUIDCharType")
     private UUID apiKey;
 
     @OneToMany(mappedBy = "customer")
