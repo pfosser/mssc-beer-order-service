10aa328c1fc7b134d988f15ad3c25346fa7696bb
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2026-01-13 09:32:42 +0100

    Lesson 224. Configure State Machine States

---
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachine.java
@@ -0,0 +1,17 @@
+package guru.sfg.beer.order.service.sm;
+
+import com.github.oxo42.stateless4j.StateMachine;
+
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class BeerOrderStateMachine {
+
+	private final StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum> machine;
+	
+	public BeerOrderStatusEnum getState() {
+		return machine.getState();
+	}
+}
---
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,32 @@
+package guru.sfg.beer.order.service.sm;
+
+import org.springframework.stereotype.Component;
+
+import com.github.oxo42.stateless4j.StateMachine;
+import com.github.oxo42.stateless4j.StateMachineConfig;
+
+import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
+import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+
+@Component
+public class BeerOrderStateMachineConfig {
+
+	public BeerOrderStateMachine create() {
+
+		BeerOrderStatusEnum initialState = BeerOrderStatusEnum.NEW;
+
+		StateMachineConfig<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineConfig = new StateMachineConfig<>();
+
+		stateMachineConfig.configure(BeerOrderStatusEnum.NEW); //
+
+		// Stati terminali
+		stateMachineConfig.configure(BeerOrderStatusEnum.PICKED_UP); //
+		stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERED); //
+		stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERY_EXCEPTION); //
+		stateMachineConfig.configure(BeerOrderStatusEnum.VALIDATION_EXCEPTION); //
+		stateMachineConfig.configure(BeerOrderStatusEnum.ALLOCATION_EXCEPTION); //
+
+		return new BeerOrderStateMachine(
+				new StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum>(initialState, stateMachineConfig));
+	}
+}
