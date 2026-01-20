96cbcba5bd7015bf847f6de7692558ad5cf7ba8c
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2026-01-20 08:58:08 +0100

    Assignment 6: Challenge - Order State Interceptor

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineFactory.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStateMachineFactory.java
@@ -4,14 +4,23 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
+import com.github.oxo42.stateless4j.StateConfiguration;
 import com.github.oxo42.stateless4j.StateMachine;
 import com.github.oxo42.stateless4j.StateMachineConfig;
 
+import guru.sfg.beer.order.service.domain.BeerOrder;
 import guru.sfg.beer.order.service.domain.BeerOrderEventEnum;
 import guru.sfg.beer.order.service.domain.BeerOrderStatusEnum;
+import guru.sfg.beer.order.service.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@RequiredArgsConstructor
 @Component
 public class BeerOrderStateMachineFactory {
+
+	private final BeerOrderRepository beerOrderRepository;
 
 	public BeerOrderStateMachine create() {
 		return createInternal(null, BeerOrderStatusEnum.NEW);
@@ -25,20 +34,44 @@ public class BeerOrderStateMachineFactory {
 
 		StateMachineConfig<BeerOrderStatusEnum, BeerOrderEventEnum> stateMachineConfig = new StateMachineConfig<>();
 
-		stateMachineConfig.configure(BeerOrderStatusEnum.NEW) //
-			.ignore(BeerOrderEventEnum.VALIDATE_ORDER) //
-			.permit(BeerOrderEventEnum.VALIDATION_PASSED, BeerOrderStatusEnum.VALIDATED) //
-			.permit(BeerOrderEventEnum.VALIDATION_FAILED, BeerOrderStatusEnum.VALIDATION_EXCEPTION);
+		StateConfiguration<BeerOrderStatusEnum, BeerOrderEventEnum> statusConfig;
+
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.NEW) //
+				.ignore(BeerOrderEventEnum.VALIDATE_ORDER) //
+				.permit(BeerOrderEventEnum.VALIDATION_PASSED, BeerOrderStatusEnum.VALIDATED) //
+				.permit(BeerOrderEventEnum.VALIDATION_FAILED, BeerOrderStatusEnum.VALIDATION_EXCEPTION);
+		addPersistence(id, statusConfig);
 
 		// Stati terminali
-		stateMachineConfig.configure(BeerOrderStatusEnum.PICKED_UP); //
-		stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERED); //
-		stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERY_EXCEPTION); //
-		stateMachineConfig.configure(BeerOrderStatusEnum.VALIDATION_EXCEPTION); //
-		stateMachineConfig.configure(BeerOrderStatusEnum.ALLOCATION_EXCEPTION); //
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.PICKED_UP); //
+		addPersistence(id, statusConfig);
+
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERED); //
+		addPersistence(id, statusConfig);
+
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.DELIVERY_EXCEPTION); //
+		addPersistence(id, statusConfig);
+
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.VALIDATION_EXCEPTION); //
+		addPersistence(id, statusConfig);
+
+		statusConfig = stateMachineConfig.configure(BeerOrderStatusEnum.ALLOCATION_EXCEPTION); //
+		addPersistence(id, statusConfig);
 
 		return new BeerOrderStateMachine(
 				new StateMachine<BeerOrderStatusEnum, BeerOrderEventEnum>(initialState, stateMachineConfig));
+	}
+
+	private StateConfiguration<BeerOrderStatusEnum, BeerOrderEventEnum> addPersistence(UUID id,
+			StateConfiguration<BeerOrderStatusEnum, BeerOrderEventEnum> config) {
+		return config.onEntry((transition) -> {
+
+			log.debug("Saving beer order with id {} and state {}", id, transition.getDestination());
+
+			BeerOrder beerOrder = beerOrderRepository.getReferenceById(id);
+			beerOrder.setOrderStatus(transition.getDestination());
+			beerOrderRepository.saveAndFlush(beerOrder);
+		}); //
 	}
 
 }
