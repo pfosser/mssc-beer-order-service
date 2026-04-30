94c556118cbd008896fb6a61fc03f551a6b07031
Paolo Fosser <paolo.fosser@sanmarcoinformatica.it>
2026-04-30 08:46:21 +0200

    Assignment 17: Assignment Eureka Client Configuration for Order and Inventory Service

--- src/main/java/guru/sfg/beer/order/service/config/JacksonConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JacksonConfig.java
@@ -1,17 +1,24 @@
 package guru.sfg.beer.order.service.config;
 
+import org.springframework.boot.jackson.autoconfigure.JsonMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import tools.jackson.databind.cfg.DateTimeFeature;
 
 @Configuration
 public class JacksonConfig {
+//	@Bean
+//	@Primary
+//	ObjectMapper objectMapper() {
+//		return new ObjectMapper().registerModule(new JavaTimeModule());
+//	}
+
 	@Bean
-	@Primary
-	ObjectMapper objectMapper() {
-		return new ObjectMapper().registerModule(new JavaTimeModule());
+	JsonMapperBuilderCustomizer jacksonCustomizer() {
+		return builder -> {
+			builder.enable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS, //
+					DateTimeFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS);
+		};
 	}
 }
--- src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/beer/BeerServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.restclient.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
